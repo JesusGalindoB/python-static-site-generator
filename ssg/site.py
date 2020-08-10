b0a5f62cd315d673c0a0e58e1a07d6179375f565
jesus <jesbaga.17@gmail.com>
2020-08-10 08:34:19 -0500

    correcting mistakes

--- ssg/site.py
+++ ssg/site.py
@@ -2,7 +2,5 @@ from pathlib import Path
 
 class Site:
     def __init__(self, source, dest):
-        source = Path()
-        dest = Path()
-        self.source = source 
-        self.dest = dest 
+        self.source = Path(source)
+        self.dest = Path(dest)
